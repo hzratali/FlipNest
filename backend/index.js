ab0916acf45c837cacf177cc825b31adb2dead66
Hazrat Ali <hazrat8143@gmail.com>
2024-09-08 01:15:12 +0530

    Cors

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,10 @@ app.use(
     credentials: true,
   })
 );
+
 // Increase payload size limit
 app.use(express.json({ limit: "50mb" }));
+
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cookieParser());
